a913e55f010bd2c5372bc103becbecf804f36063
stranothus <stranothus@gmail.com>
2023-01-23 17:03:07 -0600

    Add commands to new servers

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,10 +1,39 @@
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import dirFlat from "../utils/dirFlat.js";
+
 export default {
     type: "on",
     name: "guildCreate",
     execute: guild => {
-        global.DB.db("Info").collection("Guilds").insertOne({
-            guildID: guild.id,
-            channels: []
+
+        // load commands
+        Promise.all(dirFlat("./commands").map(async v => {
+            let imported = await import("../" + v);
+        
+            return {
+                command: v.replace(/\.[^\.]+$/, ""),
+                file: v,
+                ...imported.default
+            };
+        })).then(async commands => {
+            const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+            // register slash commands in new server
+            try {
+                await rest.put(
+                    Routes.applicationGuildCommands(client.user.id, guild.id), {
+                        body: commands.map(v => v.data.toJSON())
+                    }
+                );
+            } catch (error) {
+                console.error(error);
+            }
+
+            await global.DB.db("Info").collection("Guilds").insertOne({
+                guildID: guild.id,
+                channels: []
+            });
         });
     }
 }
